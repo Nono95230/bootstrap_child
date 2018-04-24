edc1157cbf41ad6afe4103cc5e1234917b383c90
Nono95230 <arnaud.bach1987@gmail.com>
2018-04-24 12:14:08 +0200

    [UPDATE] - Minor

--- config.rb
+++ config.rb
@@ -17,9 +17,8 @@ sass_dir = "scss"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :scss
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
